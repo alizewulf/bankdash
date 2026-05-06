da1630fb80c14170729c0363ac9ca94dd1124750
alizewulf <78692843+alizewulf@users.noreply.github.com>
2026-05-06 11:17:42 +0400

    add burger menu, fixed icons

--- src/datamodel.js
+++ src/datamodel.js
@@ -1,4 +1,7 @@
 const navListing = document.querySelector('.nav__listing')
+const burgerButton = document.querySelector('.burger-menu')
+const nav = document.querySelector('.nav')
+const header = document.querySelector('.header')
 import accounts from './assets/disabled/accounts.svg'
 import creditcards from './assets/disabled/credit-cards.svg'
 import dashboard from './assets/disabled/dashboard.svg'
@@ -14,15 +17,15 @@ const user = {
 };
 
 const routes = [
-  { id: "dashboard", label: "Dashboard", path: "/dashboard", svg: dashboard },
-  { id: "transactions", label: "Transactions", path: "/transactions", svg: transactions },
-  { id: "account", label: "Account", path: "/account", svg: accounts },
-  { id: "investments", label: "Investments", path: "/investments", svg: investments },
-  { id: "creditcards", label: "Credit Cards", path: "/creditcards", svg: creditcards },
-  { id: "loans", label: "Loans", path: "/loans", svg: loan },
-  { id: "services", label: "Services", path: "/services", svg: services },
-  { id: "privileges", label: "My Privileges", path: "/privileges", svg: privileges },
-  { id: "settings", label: "Settings", path: "/settings", svg: settings },
+  { id: "dashboard", label: "Dashboard", svg: dashboard },
+  { id: "transactions", label: "Transactions", svg: transactions },
+  { id: "account", label: "Account", svg: accounts },
+  { id: "investments", label: "Investments", svg: investments },
+  { id: "creditcards", label: "Credit Cards", svg: creditcards },
+  { id: "loans", label: "Loans", svg: loan },
+  { id: "services", label: "Services", svg: services },
+  { id: "privileges", label: "My Privileges", svg: privileges },
+  { id: "settings", label: "Settings", svg: settings },
 ];
 
 const appState = {
@@ -31,31 +34,83 @@ const appState = {
 }
 
 function renderSidebar() {
-    navListing.innerHTML = ``;
+    navListing.innerHTML = '';
     
     routes.forEach(route => {
         const li = document.createElement('li');
+
         if (route.id === appState.currentRoute) {
             li.classList.add('nav__item--active')
         } else {
-            li.classList.add('nav__item');
+            li.classList.add('nav__item')
         }
 
         li.addEventListener('click', () => {
-            appState.currentRoute = route.id;
-            renderSidebar();
-        });
+            appState.currentRoute = route.id
+            appState.sidebarOpen = false // закрываем меню на мобилке
+            renderApp()
+        })
 
-        const img = document.createElement('img');
-        img.src = route.svg;
-        img.alt = route.label;
-        li.appendChild(img);
+        const img = document.createElement('img')
+        img.src = route.svg
+        img.alt = route.label
 
-        const span = document.createElement('span');
-        span.textContent = route.label;
-        li.appendChild(span);
+        const span = document.createElement('span')
+        span.textContent = route.label
 
-        navListing.appendChild(li);
-    });
+        li.appendChild(img)
+        li.appendChild(span)
+        navListing.appendChild(li)
+    })
 }
-renderSidebar()
+
+function renderTopbar() {
+    const h2 = document.querySelector('.current-page')
+
+    const current = routes.find(r => r.id === appState.currentRoute)
+    h2.textContent = current ? current.label : ''
+}
+
+function renderSidebarVisibility() {
+    if (appState.sidebarOpen) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+
+function renderBurger() {
+    if (appState.sidebarOpen) {
+        burgerButton.classList.add('active')
+        header.classList.add('header--menu-open')
+    } else {
+        burgerButton.classList.remove('active')
+        header.classList.remove('header--menu-open')
+    }
+}
+function renderApp() {
+    renderTopbar()
+    renderSidebar()
+    renderSidebarVisibility()
+    renderBurger()
+}
+
+burgerButton.addEventListener('click', (e) => {
+    e.stopPropagation()
+    appState.sidebarOpen = !appState.sidebarOpen
+    renderApp()
+})
+
+document.addEventListener('click', (e) => {
+    const isClickInsideNav = nav.contains(e.target)
+    const isClickBurger = burgerButton.contains(e.target)
+
+    if (!isClickInsideNav && !isClickBurger) {
+        if (appState.sidebarOpen) {
+            appState.sidebarOpen = false
+            renderApp()
+        }
+    }
+})
+
+renderApp()
